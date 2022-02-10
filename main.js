c6391449373a09e442dd4da06c4223109a59f26a
Amy Gori <amydgori@gmail.com>
2022-02-10 12:31:45 -0500

    Add js for validation

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('#registration-form')
+const msgDiv = document.querySelector('#msg')
+let formIsValid // this is set as a global variable so it can be used in different functions
 
 form.addEventListener('submit', function (event) {
   event.preventDefault()
@@ -6,7 +8,52 @@ form.addEventListener('submit', function (event) {
 })
 
 function validateForm() {
-  //do something to validate form
-  let input = document.querySelector('#email-input')
-  console.log(input.value)
+  removeMessage() // if a message is showing from a previous validation attempt
+  formIsValid = true // reset this each time you try to validate the form
+
+  validateAge()
+  confirmPasswordMatch()
+  // create some more functions to validate more form fields
+
+  if (formIsValid) {
+    // submit form via AJAX
+    showMessage('Form submitted successfully!', 'success')
+  }
+}
+
+function validateAge() {
+  const ageInput = document.querySelector('#age-input')
+
+  if (ageInput.value >= 18) {
+    console.log('age is valid')
+  } else {
+    formIsValid = false
+    showMessage('You must be 18 or older', 'danger')
+  }
+}
+
+function confirmPasswordMatch() {
+  // grab the password input
+  const password = document.querySelector('#password-input')
+  // grab the confirm password input
+  const confirmPwd = document.querySelector('#confirm-password')
+  confirmPwd.parentElement.classList.remove('is-danger') // if it's there from before
+
+  // compare their values to see if they match
+  if (password.value !== confirmPwd.value) {
+    // show an error message on the page
+    showMessage('Your passwords must match', 'danger')
+    confirmPwd.parentElement.classList.add('is-danger')
+    formIsValid = false
+  }
+}
+
+function removeMessage() {
+  if (msgDiv) {
+    msgDiv.innerHTML = ''
+  }
+}
+
+function showMessage(message, state) {
+  msgDiv.innerHTML = `<div class="notification is-${state}">${message}</div>`
 }
